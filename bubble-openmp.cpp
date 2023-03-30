047520965641a549411de4797211f87d05bc376c
shaanzie <ishaanlagwankar@gmail.com>
2023-03-30 15:39:41 -0400

    Something works

---
+++ bubble-openmp.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+#include "bubble.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    int N = stoi(argv[1]);
+    int rounds = 10;
+    int max_threads = stoi(argv[2]);
+    omp_set_num_threads(max_threads);
+    double start, end, total;
+
+    vector<double> A = random_arr(N);
+
+    for(int i = 0; i < rounds; i++) {
+        start = omp_get_wtime();
+        openmp_bubble_sort(A, A.size());
+        end = omp_get_wtime();
+
+        total += end - start;
+
+    }   
+    
+    printf("%d,%d,%d,%.10f,openmp\n", max_threads, N, 1, total / rounds);
+
+    return 0;
+
+
+}
---
+++ bubble-serial.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+#include "bubble.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    int N = stoi(argv[1]);
+    int rounds = 10;
+    double start, end, total;
+
+    vector<double> A = random_arr(N);
+
+    for(int i = 0; i < rounds; i++) {
+        start = omp_get_wtime();
+        serial_bubble_sort(A, A.size());
+        end = omp_get_wtime();
+
+        total += end - start;
+
+    }
+
+    printf("%d,%d,%d,%.10f,serial\n", 1, N, 1, total / rounds);
+
+    return 0;
+
+
+}
